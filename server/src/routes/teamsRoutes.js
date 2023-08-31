6dee286ed912c5e3a592f000827b67d97386c3c0
fof3096 <c0rr1f1ll@gmail.com>
2023-08-31 20:00:09 -0300

    Handlers Team✅

--- server/src/routes/teamsRoutes.js
+++ server/src/routes/teamsRoutes.js
@@ -1,30 +1,11 @@
-const axios = require("axios");
+/* const axios = require("axios"); */
 const { Router } = require("express");
 const teamsRoutes = Router();
-const { Team } = require('../db')
+/* const { Team } = require('../db') */
+const {
+    getAllTeams
+} = require("../handlers/teamsHandlers")
 
-
-teamsRoutes.get("/",async (req,res)=>{
-    try {
-        const response = await axios.get("http://localhost:5000/drivers");
-        const { data } = response;
-        const noRepeat = new Set();
-
-        data.forEach((driver) =>{
-            if (driver.teams) {
-                let teams = driver.teams.split(",");
-                teams.forEach(team => noRepeat.add(team.trim()))
-            }            
-        })
-        await Promise.all(
-            Array.from(noRepeat).map(async (team) => {
-                await Team.findOrCreate({where : {name: team}})
-            })
-        )
-        res.json(Array.from(noRepeat));
-    } catch (error) {
-        res.status(400).json({error: error.message});
-    }
-})
+teamsRoutes.get("/",getAllTeams)
 
 module.exports = teamsRoutes;
